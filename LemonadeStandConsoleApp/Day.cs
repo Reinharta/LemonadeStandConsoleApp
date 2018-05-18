0e241b053a16cfc2b6a1e8dd17f3dced97aec167
Anina Reinhart <reinharta@outlook.com>
2018-05-17 20:06:10 -0500

    Initial Commit - classes and member variables started

---
+++ LemonadeStandConsoleApp/Day.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandConsoleApp
+{
+    class Day
+    {
+        private int salesMadeNum;
+        private int salesProceeds;
+        private int pricePerCup;
+        private int maxPotentialCustomers;
+        private int custSatisfactionPercent;
+
+
+        private int GetMaxCustomers(int highTemperature, string forecast, int custSatisfactionPercent)
+        {
+
+            return maxPotentialCustomers;
+        }
+
+        private int GetTotalSatisfaction()
+        {
+            //store each customer satisfaction percentage in list and find mean. Add daily percentage to list in Player
+
+            return custSatisfactionPercent;
+        }
+    }
+}
---
+++ LemonadeStandConsoleApp/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandConsoleApp
+{
+    class Player
+    {
+        private string playerName;
+        private int startingMoney = 20;
+        private int earnedMoney;
+        private int totalMoney;
+        private int profit;
+        private int totalCustSatisfactionPercent;
+
+
+
+
+
+
+    }
+}
